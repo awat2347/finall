30db45a10defde4ae6f9844e0c026aa51845a568
『a』『w』『a』『t』 <154649552+awat2347@users.noreply.github.com>
2025-01-16 15:00:23 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Home from './pages/Home'
 import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Pricing_Plan from './pages/Pricing_Plan'
-import Touch from './pages/Touch-page'
+import Touch_Page from './pages/Touch_page'
+
 
 
 
@@ -14,10 +15,9 @@ const App = () => {
     <>
       <Header/>
     <Routes>
-
     <Route path='/' element={<Home/>}/>
     <Route path='Pricing_Plan' element={<Pricing_Plan/>}/>
-    <Route path='Touch' element={<Touch/>}/>
+    <Route path='Touch' element={<Touch_Page/>}/>
     </Routes>
     <Footer/>
     </>
---
+++ src/pages/Touch_Page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Contact from '../Components/Contact_Us/Contact_Us'
+import Touch from '../Components/Touch/Touch'
+
+
+
+
+const Touch_Page = () => {
+  return (
+    <>
+        <Contact/>
+        <Touch/>
+    </>
+  )
+}
+
+export default Touch_Page
