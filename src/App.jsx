21ead6acf36743d6d6fdc207479744811cd730e7
『a』『w』『a』『t』 <154649552+awat2347@users.noreply.github.com>
2025-01-18 00:29:40 -1200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Header from './Components/Header/Header'
 import Footer from './Components/Footer/Footer'
 import Pricing_Plan from './pages/Pricing_Plan'
 import Touch_Page from './pages/Touch_page'
+import Erorr from './Components/erorr_wrapp/erorr'
+
 
 
 
@@ -18,6 +20,7 @@ const App = () => {
     <Route path='/' element={<Home/>}/>
     <Route path='Pricing_Plan' element={<Pricing_Plan/>}/>
     <Route path='Touch' element={<Touch_Page/>}/>
+    <Route path='*' element={<Erorr/>}/>
     </Routes>
     <Footer/>
     </>
--- src/Components/erorr_wrapp/erorr.jsx
+++ src/Components/erorr_wrapp/erorr.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import s from './erorr.module.scss'
-const erorr = () => {
+const Erorr = () => {
   return (
     <>
        <section>
@@ -14,10 +14,18 @@ const erorr = () => {
                 <h1>Page Not Found</h1>
                 </div>
                     <div className={s.Oops}>
-                    <p></p>
+                    <p>Oops! It looks like the page you were looking for is not here. Here are some possible reasons why:</p>
                     <ul>
-                        <li></li>
+                        <li>The page may have been moved or deleted.</li>
+                        <li>You may have mistyped the URL.</li>
+                        <li>
+There may be a temporary problem with our server.
+You can try the following options to find what you're looking for:</li>
+                        <li>Check the URL for typos or errors and try again.</li>
+                        <li>Go back to our homepage and browse from there.</li>
                     </ul>
+                    <p>If you believe there's an issue with our website, please contact us using the information provided on our contact page</p>
+<button>back home</button>
                     </div>
                 </div>
             </div>
@@ -27,4 +35,4 @@ const erorr = () => {
   )
 }
 
-export default erorr
+export default Erorr
